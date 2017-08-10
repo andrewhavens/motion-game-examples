bb5073eac2a043644c1e77d9f563543c720f5584
Andrew Havens <email@andrewhavens.com>
2017-08-09 23:29:39 -0700

    WIP: add tile map example

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'rake'
-gem 'motion-game', '~> 1.1.14'
-# gem 'motion-game', path: '../motion-game'
+# gem 'motion-game', '~> 1.1.14'
+gem 'motion-game', path: '../motion-game'
--- app/sprites/character.rb
+++ app/sprites/character.rb
@@ -15,16 +15,24 @@ class Character < MG::Sprite
     end
   end
 
+  def time_to_walk_to(new_location)
+    distance = position.distance(new_location)
+    distance / 150 # move at a rate of 150 points per second
+  end
+
   def walk_to(new_location, &block)
     stop_all_actions # cancel any moves in progress since our destination has changed
-    distance = position.distance(new_location)
-    speed = distance / 150 # move at a rate of 150 points per second
     self.flipped_horizontally = moved_left?(new_location)
-    move_to(new_location, speed)
+    duration = time_to_walk_to(new_location)
+    move_to(new_location, duration)
+    run_walk_animation(duration, &block)
+  end
+
+  def run_walk_animation(duration, &block)
     frames = [1, 2, 3, 4, 3, 2].map {|i| "hero_walk#{i}.png"}
     animate(frames, 0.15, MG::Repeat::FOREVER)
     # FIXME: use a Sequence instead of a DelayTime action
-    run_action(MG::DelayTime.new(speed)) do
+    run_action(MG::DelayTime.new(duration)) do
       stop_all_actions # stop walking animation
       animate(["hero.png"], 1, MG::Repeat::FOREVER)
       block.call if block
